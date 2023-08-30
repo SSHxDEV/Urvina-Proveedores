f46725183a9d60d08dd5c5b4bbc2164ca3b9c34b
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-08-30 11:18:47 -0600

    Visualizacion factura en SHOW

--- resources/views/facturas/consulta/factura-show.blade.php
+++ resources/views/facturas/consulta/factura-show.blade.php
@@ -220,7 +220,7 @@
         <div class="card-body">
             <div style="padding:20px;background-color: RGBA( 0 , 255 , 0 , 0.2 );" class="row">
                 <div class="col-4"><img class="grow" src="/icons/xml.png" width="60px" alt=""></div>
-                <div class="col-8"><b>{{__('Documento XML de Factura')}}</b><br><a class="btn btn-primary btn-xml" data-rfc="{{$factura->receptor}}" data-file="{{$factura->factura}}"> {{__('Visualizar XML')}}</a></div>
+                <div class="col-8"><b>{{__('Documento XML de Factura')}}</b><br><a class="btn btn-primary btn-xml" data-rfc="{{$data[0]->receptor}}" data-file="{{$data[0]->factura}}"> {{__('Visualizar XML')}}</a></div>
             </div>
 
             {{-- UTILIZAR CODIGO EN CASO DE USAR UN PDF APARTE DEL SELLADO
@@ -269,7 +269,7 @@
             <div style="padding:20px;background-color: RGBA( 0 , 255 , 0 , 0.2 );" class="row">
 
                 <div class="col-4"><a></a><img class="grow" src="/icons/pdf.png" width="60px" alt=""></div>
-                <div class="col-8"><b>{{__('Documento PDF de Factura')}}</b><br><a class="btn btn-primary btn-file" onclick="cargarPDF('{{$factura->receptor}}/{{$_SESSION['usuario']->RFC}}/{{$factura->PDFsello}}.pdf')" > {{__('Visualizar PDF')}} </a> </div>
+                <div class="col-8"><b>{{__('Documento PDF de Factura')}}</b><br><a class="btn btn-primary btn-file" onclick="cargarPDF('{{$data[0]->receptor}}/{{$_SESSION['usuario']->RFC}}/{{$data[0]->PDFsello}}.pdf')" > {{__('Visualizar PDF')}} </a> </div>
             </div>
 
             @endif
