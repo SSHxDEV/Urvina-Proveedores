a946c4e9924bab4e87470b8095005b31d8e8a85d
Carlos Enrique Guizar Peña <71019682+Skullnietz@users.noreply.github.com>
2023-09-07 15:11:47 -0600

    Ajustes de Errores

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,1 +1,5 @@
-Hello
+@extends('errors::minimal')
+
+@section('title', __('No disponible'))
+@section('code', '500')
+@section('message', __('El servicio no está disponible en este momento; por favor, inténtelo nuevamente más tarde.'))
